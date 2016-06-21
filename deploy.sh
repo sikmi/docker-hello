cba7c554f0f7228babe28859dd9a3934708c4a0e
taru-m <wwww.0909mizu@gmail.com>
2016-06-21 19:44:57 +0900

    region指定を追加

--- deploy.sh
+++ deploy.sh
@@ -10,8 +10,8 @@ docker push circleci/hello:$SHA1
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
-aws elasticbeanstalk create-application-version --application-name hello \
+aws elasticbeanstalk create-application-version --region ap-northeast-1 --application-name hello \
     --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 
 # Update Elastic Beanstalk environment to new version
-aws elasticbeanstalk update-environment --environment-name hello-env --version-label $SHA1
+aws elasticbeanstalk update-environment --region ap-northeast-1 --environment-name hello-env --version-label $SHA1
